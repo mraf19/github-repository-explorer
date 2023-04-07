b5fe60bb0baa1369bb626c65abdc5b6750bec521
mraf19 <raflimr1902@gmail.com>
2023-04-07 09:13:38 +0700

    edit code

--- src/components/ContainerList.tsx
+++ src/components/ContainerList.tsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import { UserList } from "./UserList";
-import { ChevronDownIcon } from "@heroicons/react/24/solid";
-import { UserRepositories } from "./UserRepositories";
 
 type ContainerListProps = {
   users: {
@@ -10,19 +7,6 @@ type ContainerListProps = {
   query: string;
 };
 export const ContainerList = ({ users, query }: ContainerListProps) => {
-  const [show, setShow] = useState(false);
-  const [repos, setRepos] = useState([]);
-  const onClick = async (username: string) => {
-    const query = await fetch(`https://api.github.com/users/${username}/repos`);
-    const res = await query.json();
-    const repos = res.map((r: any) => {
-      return {
-        name: r.name,
-        description: r.description,
-        stargazers_count: r.stargazers_count,
-      };
-    });
-  };
   return (
     <div className="w-full mt-[20px] text-left text-zinc-400 flex flex-col space-y-3">
       <h4 className="text-bold text-xl">Showing user for "{query}"</h4>
